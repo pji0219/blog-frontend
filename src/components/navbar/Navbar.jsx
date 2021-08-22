ea340737919814ddb3fb01d4195b45b4b04c1a10
PJI-LAPTOP\admin <PJI-LSPTOP\admin>
2021-08-22 17:06:58 +0900

    사이드 메뉴바 토글 형식으로 변경

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import styles from './Navbar.module.css';
-import { FaSignInAlt } from "react-icons/fa";
+import classNames from 'classnames';
+import { FaSignInAlt, FaBars } from "react-icons/fa";
+import { AiOutlineClose } from 'react-icons/ai';
 
 function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
   return (
     <>
       <header className={styles.header}>
+        <Link to="#" className={styles.open_menu_bar_btn}>
+          <FaBars onClick={showSidebar} />
+        </Link>
         <span className={styles.blog_name}>Blog</span>
         <span className={styles.login_btn}>
           <FaSignInAlt />
         </span>
       </header>
-      <nav className={styles.nav_menu}>
+      <nav className={sidebar ? classNames({[styles.nav_menu]: true, [styles.active]: true}) : styles.nav_menu}>
         <ul className={styles.nav_menu_items}>
+          <li className={styles.navbar_toggle}>
+            <Link to="#" className={styles.close_menu_bar_btn}>
+              <AiOutlineClose onClick={showSidebar}/>
+            </Link>
+          </li>
           {
             SidebarData.map((item, index) => {
               return (
